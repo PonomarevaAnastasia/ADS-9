9cd7b2a774b414e7448ccc45072d1b6bdb45cc20
PonomarevaAnastasia <116709050+PonomarevaAnastasia@users.noreply.github.com>
2023-06-04 18:06:20 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -7,4 +7,33 @@
 
 BST<std::string> makeTree(const char* filename) {
   // поместите сюда свой код
+  std::ifstream file(filename);
+  BST<std::string> tree;
+  std::string word;
+
+  if (file) {
+    while (!file.eof()) {
+      work.erase();
+      while (1) {
+        char sum = file.get();
+        if (sum > 96 && sum < 123) {
+          word += symbol;
+        } else if (sum > 64 && sum < 91) {
+            sum += 32;
+            work += sum;
+        } else {
+          break;
+        }
+      }
+      if (!work.empty()) {
+        tree.addNode(word);
+      } else {
+        continue;
+      }
+    }
+    file.close();
+  } else {
+    std::cout << "File was not read correctly." << std::endl;
+  }
+  return tree;
 }
